866e9b9defeb04bbd65c57a803c59dbe8416710b
George Garcia <jorge.90gap@gmail.com>
2021-11-12 18:50:44 +0900

    Setting Routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'stations/index'
-  get 'stations/show'
-  get 'stations/new'
-  get 'stations/edit'
-  get 'properties/index'
-  get 'properties/show'
-  get 'properties/new'
-  get 'properties/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'properties#index'
+  resources :stations
+  resources :properties
 end
