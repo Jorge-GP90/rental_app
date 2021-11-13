abe432c4310a8af27013ab3db85a8e3396048a80
George Garcia <jorge.90gap@gmail.com>
2021-11-13 16:41:17 +0900

    Debug gem were installed

--- config/application.rb
+++ config/application.rb
@@ -7,15 +7,13 @@ Bundler.require(*Rails.groups)
 module DevExam
   class Application < Rails::Application
     config.load_defaults 5.2
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :en
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.generators do |g|
       g.assets false
       g.helper false
       g.jbuilder false
     end
-
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
   end
 end
