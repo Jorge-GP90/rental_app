97b16aa0e0b569bab596df6f9f50d91baba5b69d
George Garcia <jorge.90gap@gmail.com>
2021-11-13 16:40:40 +0900

    Controller properties and Stations was Setted up

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,13 +1,61 @@
 class PropertiesController < ApplicationController
+  before_action :set_property, only: %i[ show edit update destroy]  
   def index
+    @properties = Property.all
   end
 
   def show
+    @stations = @property.stations
   end
 
   def new
+    @property = Property.new
+    2.times {@property.stations.build}
   end
 
   def edit
+    @property.stations.build
+  end
+  
+  def create
+    @property = Property.new(property_params)
+    respond_to do |format|
+      if @property.save
+        format.html { redirect_to @property, notice: "Your Property has been register successfully." }
+        format.json { render :show, status: :created, location: @property }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @property.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @property.update(property_params)
+        format.html { redirect_to @property, notice: "Your Property has been updated successfully." }
+        format.json { render :show, status: :ok, location: @property }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @property.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @property.destroy
+    respond_to do |format|
+      format.html { redirect_to station_url, notice: " Property was eliminated." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:property_name, :rent, :address, :age, :remarks, stations_attributes: [:id, :line, :station_name, :minutes, :_destroy])
   end
 end
--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,13 +1,58 @@
 class StationsController < ApplicationController
+  before_action :set_station, only: %i[ show edit update destroy]
   def index
+    @station = Station.all
   end
 
   def show
   end
 
   def new
+    @staion = Station.new
   end
 
   def edit
+  end
+
+  def create
+    @station = Station.new(station_params)
+    respond_to do |format|
+      if @station.save
+        format.html { redirect_to @station, notice: " Station was successfully created." }
+        format.json { render :show, status: :created, location: @station }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @station.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def update
+    respond_to do |format|
+      if @station.update(station_params)
+        format.html { redirect_to @station, notice: " Station was successfully updated." }
+        format.json { render :show, status: :ok, location: @station }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @station.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+    @station.destroy
+    respond_to do |format|
+        format.html { redirect_to station_url, notice: " Station was successfully destroyed." }
+        format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_station
+    @station = Station.find(params[:id])
+  end
+  
+  def station_params
+    params.require(:station).permit(:line, :station_name, :minutes)
   end
 end
